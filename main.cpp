bcdd5765e5172f84e9a8e33bbef4a5af3dfa9b0a
OppositeFool <39835908+OppositeFool@users.noreply.github.com>
2022-09-19 22:22:15 +0200

    commented errors

--- main.cpp
+++ main.cpp
@@ -23,25 +23,4 @@ int main()
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
-
-
-    int* b = new int[N_ELEMENTS];
-    std::cout << "1-100 ertekek duplazasa" << std::endl;
-        for (int i = 1;i<N_ELEMENTS+1;i++)
-        {
-            b[i-1] = i * 2;
-        }
-    for (int i = 0; i<=N_ELEMENTS-1; i++)
-    {
-        std::cout << "Ertek:" << b[i] << std::endl;
-    }
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag=0;
-    for (int i = 0; i < N_ELEMENTS; i++)
-    {
-        atlag += b[i];
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
 }
